4e1048547db854e2a12a2c8b6552a8cf44abc1f5
zamurvedovattosymplicity <zvedovatto@symplicity.com>
2021-07-02 22:59:08 -0300

    header adjs

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,12 +5,13 @@ export default function Navbar() {
     const router = useRouter()
     return (
         <nav>
-            <div className="flex justify-center pt-6 pb-3 px-5 sm:px-10 whitespace-nowrap space-x-5 sm:space-x-10 overflow-x-scroll scrollbar-hide bg-black">
+            <div className="flex flex-wrap px-10 py-6 justify-around items-center">
+            {/* <div className="flex justify-center pt-6 pb-3 px-5 sm:px-10 whitespace-nowrap space-x-5 sm:space-x-10 overflow-x-scroll scrollbar-hide bg-black"> */}
                 {Object.entries(requests).map(([key, { title, url }]) => (
                     <span
                         key={key}
                         onClick={() => router.push(`/?genre=${key}`)}
-                        className="cursor-pointer transition-all duration-100 transform hover:scale-125 hover:text-white text-base"
+                        className="cursor-pointer transition-all duration-100 transform hover:scale-125 hover:text-white text-base m-2"
                     >#{title}</span>
                 ))}
             </div>
